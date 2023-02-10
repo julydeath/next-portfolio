051302cfc9651221c9c8824e44ca87a052559a88
julydeath <manojkarajada.mk@gmail.com>
2023-02-10 13:32:02 +0530

    chore: update meta tags

--- components/projects/More.tsx
+++ components/projects/More.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function More() {
     return (
       <p className="py-20 text-sm italic text-fun-gray">
-        Hey, hey, hey... I've got even more on <a className="text-fun-pink underline" href="https://github.com/braydentw" target="_blank">my GitHub</a>!
+        Hey, hey, hey... I've got even more on <a className="text-fun-pink underline" href="https://github.com/julydeath" target="_blank">my GitHub</a>!
       </p>
     );
 }
