ca52e5cf3fa00d18aeea58fce9c6bea733376cef
julydeath <manojkarajada.mk@gmail.com>
2023-02-10 16:13:14 +0530

    chore : footer edited

--- components/global/Footer.tsx
+++ components/global/Footer.tsx
@@ -88,7 +88,7 @@ function Footer() {
       <div className="mt-8 text-center sm:text-right sm:-mt-12">
         <a
           className="w-auto inline-flex items-center sm:w-auto font-bold flex-shrink text-xs border border-fun-pink px-4 py-2 rounded-xl text-fun-pink cursor-pointer opacity-50"
-          href="https://github.com/julydeath/next-tailwind-typescript-starter"
+          href="https://github.com/julydeath/next-portfolio"
           target="_blank"
           rel="nooreferrer"
         >
