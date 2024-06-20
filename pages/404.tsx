4e9cfca5bc8fda898093ad0f4a0be19a8db5e056
julydeath <manojkarajada.mk@gmail.com>
2024-06-20 23:20:28 +0530

    chore : change in ui

--- pages/404.tsx
+++ pages/404.tsx
@@ -16,7 +16,7 @@ function Page404() {
         <div className="min-h-screen w-full flex items-center justify-center flex-col animate-fadeIn">
           <h1 className="text-7xl text-white font-monospace font-bold opacity-100">{`{ error: ${num404} }`}</h1>
           <p className="text-fun-gray text-xl mt-8 flex items-center">
-            Sorry, looks like that page is missing!&nbsp;&nbsp;
+            Sorry, looks like that page is gone!&nbsp;&nbsp;
             <Link href="/">
               <span className="w-full sm:w-auto flex-shrink border border-fun-pink-light text-base px-4 py-1 rounded-xl text-fun-pink-light bg-fun-pink-darkerer hover:bg-fun-pink hover:text-white transition-colors cursor-pointer">
                 Return Home
